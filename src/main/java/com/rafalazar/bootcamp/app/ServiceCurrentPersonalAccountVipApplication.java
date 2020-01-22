9e321219cf38ff1b69c159d2e98090d62cb80fa0
Jordan <jcr.salazar@outlook.com>
2020-01-22 14:49:38 -0500

    MS creado.

---
+++ src/main/java/com/rafalazar/bootcamp/app/ServiceCurrentPersonalAccountVipApplication.java
@@ -0,0 +1,13 @@
+package com.rafalazar.bootcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCurrentPersonalAccountVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCurrentPersonalAccountVipApplication.class, args);
+	}
+
+}
