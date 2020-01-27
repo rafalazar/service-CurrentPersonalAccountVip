582e6e6c070c8291b66191dc9f7768a2e546a213
Jordan <jcr.salazar@outlook.com>
2020-01-27 10:35:50 -0500

    ServiceImpl separado.

--- src/main/java/com/rafalazar/bootcamp/app/service/CurrentAccountVipServiceImpl.java
+++ src/main/java/com/rafalazar/bootcamp/app/service/CurrentAccountVipServiceImpl.java
@@ -1,4 +1,4 @@
-package com.rafalazar.bootcamp.app.service;
+	package com.rafalazar.bootcamp.app.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
